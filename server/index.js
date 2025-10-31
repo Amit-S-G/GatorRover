bf06bab0e3be4cdcea8053608160d9743152df2d
PhillipsCorey <coreypofficial@gmail.com>
2025-10-31 16:51:57 -0400

    updated server endpoints to create /stream and /upload for use with video streaming

--- server/index.js
+++ server/index.js
@@ -13,15 +13,39 @@ if (process.env.NODE_ENV === "LOCAL") {
   console.log("Starting VM setup.");
 }
 
-app.use(
-  "/",
+// this is the latest frame, we'll keep this updated whenever
+// '/upload' is hit, and return that image whenever '/stream'
+// is hit.
+let latestFrame = null;
+
+// the stream endpoint (user accessible) requires basic auth from the .env file
+// and returns the jpeg image stored above once authenticated
+app.get(
+  "/stream",
   basicAuth({
     users: { [browser_username]: browser_password },
     challenge: true
-  })
+  }),
+  (req, res) => {
+    if (!latestFrame) {
+      return res.status(404).send("No frame yet");
+    }
+    res.set("Content-Type", "image/jpeg");
+    res.send(latestFrame);
+  }
 );
-app.get("/", (req, res) => {
-  res.send("Hello, authorized user! You are now seeing the page.");
+
+// the upload endpoint (ESP32 accessible) requires an api key from the .env file
+// and allows the caller to update the latestFrame with an uploaded jpg image
+app.use('/upload', express.raw({ type: 'image/jpeg', limit: '2mb' })); 
+app.post('/upload', (req, res) => {
+  const auth = req.headers['authorization'];
+  if (!auth || auth !== `Bearer ${esp32_api_key}`) { // this checks to make sure the correct api key is sent
+    return res.status(401).send('Unauthorized');
+  }
+
+  latestFrame = req.body;
+  res.send('Frame received');
 });
 
 
