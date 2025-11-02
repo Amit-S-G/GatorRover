457106a77310950570b7fee8e1fa7a0421abe1df
Cfengsu2002 <Cfengsu2002@gmail.com>
2025-11-02 01:08:58 -0500

    Add socket simulator and finished the drone wifi connection

--- socket_simulator/client.py
+++ socket_simulator/client.py
@@ -2,26 +2,43 @@ import socket
 import time
 import random
 
-SERVER_IP = "127.0.0.1"   
-SERVER_PORT = 8080    
+SERVER_IP = "192.168.4.1"
+SERVER_PORT = 8080
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER_IP, SERVER_PORT))
-print(f"🤝 Connected to Car Server at {SERVER_IP}:{SERVER_PORT}")
+
+try:
+    client.connect((SERVER_IP, SERVER_PORT))
+    print(f"Connected to ESP32 {SERVER_IP}:{SERVER_PORT}")
+except Exception as e:
+    print("Connection failed")
+    print(e)
+    exit()
 
 try:
     while True:
         left_voltage = random.randint(0, 4095)
         right_voltage = random.randint(0, 4095)
 
-        msg = f"L:{left_voltage} R:{right_voltage}\n"
+        def get_action(v):
+            if v <= 30:
+                return "forwards"
+            elif v >= 1100:
+                return "backwards"
+            else:
+                return "stop"
 
+        left_action = get_action(left_voltage)
+        right_action = get_action(right_voltage)
+
+        print(f"Left: {left_voltage:4d} ({left_action}) | Right: {right_voltage:4d} ({right_action})")
+
+        msg = f"L:{left_voltage} R:{right_voltage}\n"
         client.send(msg.encode())
 
-        print(f"Sent: {msg.strip()}")
-
-        time.sleep(2)  
+        time.sleep(0.5)
 except KeyboardInterrupt:
     print("Client stopped.")
 finally:
     client.close()
+    print("Disconnected.")
