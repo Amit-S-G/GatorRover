9535f34ad2abc884d68dd112680fc2ef9a1babe0
PhillipsCorey <coreypofficial@gmail.com>
2025-11-02 13:34:47 -0500

    fixed url issue in script.js

--- server/public/script.js
+++ server/public/script.js
@@ -2,8 +2,7 @@ const img = document.getElementById("camera");
 
 // Poll /frame every 200ms
 setInterval(() => {
-  // the use of ?t= does nothing for the stream endpoint
-  // however, it is implemented to prevent cacheing from
-  // causing stream issues.
-  img.src = "/stream?t=" + new Date().getTime();
+  // the new etag header means a ?t path variable
+  // isn't necessary to get over caching issues.
+  img.src = "/stream"
 }, 5000);
