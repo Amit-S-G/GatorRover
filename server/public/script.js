c26d4e9e3a5fb1415e90500df0ad2042e0dfb3a4
PhillipsCorey <coreypofficial@gmail.com>
2025-11-07 10:59:55 -0500

    updated some of alex's styling

--- server/public/script.js
+++ server/public/script.js
@@ -2,11 +2,11 @@ const img = document.getElementById("camera");
 const statusText = document.getElementById("status");
 
 img.onload = () => {
-  statusText.textContent = "✅ Visual link established!";
+  statusText.textContent = "Visual link established!";
   statusText.style.color = "#00ff99";
 };
 
 img.onerror = () => {
-  statusText.textContent = "⚠️ No signal detected!";
+  statusText.textContent = "No signal detected!";
   statusText.style.color = "#ff4444";
 };
