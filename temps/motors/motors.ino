f4fed5e2ff628a0eb13fee64bae0e9a4360f0ed0
Amit "Sunny" Gupta <137424796+Amit-S-G@users.noreply.github.com>
2025-11-07 12:26:34 -0500

    Update motors.ino

--- temps/motors/motors.ino
+++ temps/motors/motors.ino
@@ -38,7 +38,7 @@ void setup() {
   
   Serial.begin(115200);
   while (!Serial);
-  
+  // set up nrf with channel six seven
   if (!nrf24.init()) Serial.println("init failed");
   if (!nrf24.setChannel(67)) Serial.println("setChannel failed");
   if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) Serial.println("setRF failed");
@@ -53,7 +53,7 @@ void loop() {
     uint8_t len = sizeof(code);
     if (nrf24.recv(&code, &len)) {
       fromCode(code, leftWheel, rightWheel);
-      
+      // print what's receieved
       Serial.print("Received - LEFT: ");
       Serial.print(leftWheel);
       Serial.print(" | RIGHT: ");
